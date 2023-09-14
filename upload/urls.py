54185f8464cdeb28bc81c8df852ed22f228938a6
Md. Jabed Ahmed <jabedahmedrifat1@gmail.com>
2023-09-14 14:39:40 +0600

    commit

--- upload/urls.py
+++ upload/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('excel/', export_import_excel ),
-    path('create/', createStudent)
+    path('create/', createStudent),
 ]
