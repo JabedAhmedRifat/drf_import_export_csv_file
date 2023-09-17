7c3be52d3672c0293b21a031ed97ebf876c2d1e3
Md. Jabed Ahmed <jabedahmedrifat1@gmail.com>
2023-09-17 14:40:11 +0600

    delete after save

--- upload/views.py
+++ upload/views.py
@@ -6,6 +6,8 @@ import uuid
 from .models import *
 from .serializers import *
 from django.conf import settings
+import os
+import time
 
 
 @api_view(['POST'])
@@ -23,12 +25,19 @@ def export_import_excel(request):
     if request.method == 'GET':
         stu_obj = Student.objects.all()
         serializer = StudentSerializer(stu_obj, many=True)
-        # return Response(serializer.data)
         df = pd.DataFrame(serializer.data)
         print(df)
         file_path = f'public/static/excel/{uuid.uuid4()}.csv'
         df.to_csv(file_path, encoding='UTF-8')
+        
+        
+        time.sleep(60)  
+        
+        if os.path.exists(file_path):
+            os.remove(file_path)
         return Response({'status': 200})
+        
+        
 
     elif request.method == 'POST':
         excel_file = request.FILES.get('files')
@@ -37,6 +46,7 @@ def export_import_excel(request):
             file_path = f'{settings.BASE_DIR}/public/static/{exceled_upload_obj.excel_file_upload}'
             df = pd.read_csv(file_path)
             print(df.values.tolist())
+            # os.delete(file_path)
             return Response({'status': 200})
         else:
             return Response({'status': 400})
